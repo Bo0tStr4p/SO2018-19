1321358677a98f2ba052b1804c0d55211f530a12
Appolloni21 <alessandro.appolloni21@gmail.com>
2019-05-09 00:15:46 +0200

    bitmap_test.c

--- Makefile
+++ Makefile
@@ -23,8 +23,8 @@ all:	$(BINS)
 so_game: simplefs_test.c $(OBJS) 
 	$(CC) $(CCOPTS) -o $@ $^ $(LIBS)
 	
-test: simplefs_test.c bitmap.c $(HEADERS)
-				$(CC) $(CCOPTS) simplefs_test.c bitmap.c -o test
+bitmap_test: bitmap_test.c bitmap.c $(HEADERS)
+				$(CC) $(CCOPTS) bitmap_test.c bitmap.c -o bitmap_test
 
 clean:
 	rm -rf *.o *~  $(BINS)
--- simplefs_test.c
+++ simplefs_test.c
@@ -2,30 +2,10 @@
 #include "bitmap.h"
 #include <stdio.h>
 
-int main(int agc, char** argv) { 						//A. Main di prova per testare le funzioni di bitmap.c e diskdriver.c
-	//printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
-	//printf("DataBlock size %ld\n", sizeof(FileBlock));
-	//printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
-	//printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));
+int main(int agc, char** argv) { 						
+	printf("FirstBlock size %ld\n", sizeof(FirstFileBlock));
+	printf("DataBlock size %ld\n", sizeof(FileBlock));
+	printf("FirstDirectoryBlock size %ld\n", sizeof(FirstDirectoryBlock));
+	printf("DirectoryBlock size %ld\n", sizeof(DirectoryBlock));
   
-	printf("--------------- 1. TEST BITMAP.C --------------------\n\n");
-  
-	printf("--------------- 1.1 TEST BITMAP BLOCK TO INDEX -------\n");
-  
-	int i;
-  
-	for(i=0; i<32; i++){
-		BitMapEntryKey key1 = BitMap_blockToIndex(i);
-		printf("Block:%i -> Byte:%i - Bit:%i\n",i, key1.entry_num, key1.bit_num);
-	}
-  
-	printf("\n--------------- 1.2 TEST BITMAP INDEX TO BLOCK -------\n");
-	
-	int j;
-	for(i=0; i<3; i++){
-		for(j=0; j<3; j++){
-			int block = BitMap_indexToBlock(i,j);
-			printf("Byte:%i - Bit:%i -> Block:%i\n", i, j, block);
-		}
-	}
 }
